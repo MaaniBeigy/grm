bf9b1bb6b3987bef44c68045aae458e53e97d591
MaaniBeigy <manibeygi@gmail.com>
2019-07-04 19:16:43 +0430

    :tada: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// grm
+// Generalized Resemblance Models
+// Copyright: 2018, Maani Beigy <manibeygi@gmail.com>, 
+// License: MIT/Apache-2.0
+//! # grm
+//!
+//! `gem` is a machine learning algorithm which dtands for Generalized 
+//! Resemblance Models. It is a multi-layer similarity-based learning method.
+//! 
